257af4238ca564ec05983998e250b0caebea1afd
elliot_skt <1113193@sktelecom.com>
2022-01-25 15:36:04 +0900

    change mapping methods in course class to static

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,25 +1,24 @@
 package pairmatching.domain;
+
 import java.util.Arrays;
 
 public enum Course {
-    BACKEND("백엔드"),
-    FRONTEND("프론트엔드");
+    BACKEND("백엔드"), FRONTEND("프론트엔드");
 
     private String name;
 
-    Course(String name){
+    Course(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
         return name;
     }
 
-    public Course mappingStringValue(String courseName){
-        return Arrays.stream(Course.values())
-        .filter(course -> course.getName().equals(courseName))
-        .findFirst()
-        .orElseThrow( () -> new IllegalArgumentException("[ERROR] 과정을 잘못 입력하셨습니다."));
+    public static Course mappingStringValue(String courseName) {
+        return Arrays.stream(Course.values()).filter(course -> course.getName().equals(courseName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 과정을 잘못 입력하셨습니다."));
     }
-    
+
 }
