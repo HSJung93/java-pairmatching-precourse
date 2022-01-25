0a8d4970cd7b21532fefceb89f9f634c100a1683
elliot_skt <1113193@sktelecom.com>
2022-01-25 15:34:48 +0900

    add mapping methods in level class

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,17 +1,25 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),    
-    LEVEL3("레벨3"),    
-    LEVEL4("레벨4"),    
-    LEVEL5("레벨5");
-    
+    LEVEL1("레벨1"), LEVEL2("레벨2"), LEVEL3("레벨3"), LEVEL4("레벨4"), LEVEL5("레벨5");
+
     private String name;
 
-    Level(String name){
+    Level(String name) {
         this.name = name;
     }
 
-    // TODO: add features
+    public String getName() {
+        return name;
+    }
+
+    public static Level mappingStringValue(String levelName) {
+        return Arrays.stream(Level.values()).filter(level -> level.getName().equals(levelName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 레벨을 잘못 입력하셨습니다."));
+    }
+
+
 }
--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -15,27 +15,22 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 짝수_인원_페어_매칭() {
-        assertShuffleTest(
-            () -> {
-                run("1", "백엔드, 레벨1, 자동차경주", "Q");
-                assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
-            },
-            Arrays.asList("태웅", "백호", "치수", "태섭")
-        );
+        assertShuffleTest(() -> {
+            run("1", "백엔드, 레벨1, 자동차경주", "Q");
+            assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
+        }, Arrays.asList("태웅", "백호", "치수", "태섭"));
     }
 
     @Test
     void 없는_미션에_대한_예외_처리() {
-        assertSimpleTest(
-            () -> {
-                runException("1", "백엔드, 레벨1, 오징어게임");
-                assertThat(output()).contains(ERROR_MESSAGE);
-            }
-        );
+        assertSimpleTest(() -> {
+            runException("1", "백엔드, 레벨1, 오징어게임");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 }
