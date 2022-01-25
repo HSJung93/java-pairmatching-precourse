a0998c6bec58574475ba8c94fd639cdc3decb5ce
elliot_skt <1113193@sktelecom.com>
2022-01-25 17:35:17 +0900

    add OuputView

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -5,17 +5,19 @@ import java.util.Objects;
 public class Crew {
     private Course course;
     private String name;
-    
-    public Crew(Course course, String name){
+
+    public Crew(Course course, String name) {
         this.course = course;
         this.name = name;
     }
 
     @Override
-    public boolean equals(Object o){
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Crew crew = (Crew)o;
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Crew crew = (Crew) o;
         return Objects.equals(name, crew.name);
     }
 
@@ -24,5 +26,9 @@ public class Crew {
         return Objects.hash(name);
     }
 
-    
+    public String getName() {
+        return this.name;
+    }
+
+
 }
---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,20 @@
+package pairmatching.view;
+
+import pairmatching.domain.Crew;
+
+public class OutputView {
+    private static final String PAIR_MATCHING_RESULT = "페어 매칭 결과입니다.";
+    private static final String CREW_SPLIT_LINE = " : ";
+
+    public static void alarmPairMatchingResult() {
+        System.out.println();
+    }
+
+    public static void showPairMatchingResult(List<Crew> crewList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Crew crew : crewList) {
+            stringBuilder.append(crew.getName()).append(CREW_SPLIT_LINE);
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
